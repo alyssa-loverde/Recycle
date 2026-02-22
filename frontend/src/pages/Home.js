b323dc22ed24c8d4d9a5737c101f93d4df75e3f5
aasimon2 <aasimon2@illinois.edu>
2026-02-22 17:40:57 -0600

    home stuff

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
-import './ItemList.css';
+import './Home.css';
 
 function Home(){
   return (
-    <div className="ItemList">
+    <div className="Home">
         <h1>Learn How to Properly Recycle</h1>
         <p>Recycling does not have to be confusing. Our mission is to help individuals understand all about recycling and how easy it can be. Explore the codes, common items, and nearby drop-off locations to recycle responsibly.</p>
+      <img 
+        src="https://as1.ftcdn.net/jpg/07/97/85/80/1000_F_797858052_F9vvtqZWA4qfkuTaxe1vhfjG7vJh2h0b.jpg" 
+        alt="recycling bins" 
+        style={{ width: "300px" }}
+      />
     </div>
   )
 }
