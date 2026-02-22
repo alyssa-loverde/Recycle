555cf9cf992e0ceaf482e367874d03c0ad5697ad
Pranet <pranetkumar2007@gmail.com>
2026-02-22 12:55:27 -0600

    Removed id and made name unique

--- app.py
+++ app.py
@@ -1,35 +1,55 @@
 import csv
 import sqlite3
+from flask import Flask, redirect, render_template, request, url_for
+
+app= Flask(__name__)
+
 
 db = sqlite3.connect("users.db")
 cursor = db.cursor()
 
+@app.route("/")
 def setup_database():
     db = sqlite3.connect("users.db")
     cursor.execute('''
                     CREATE TABLE IF NOT EXISTS users (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    name VARCHAR(50) NOT NULL,
+                    name VARCHAR(50) UNIQUE NOT NULL,
                     points INT
                 ) ''')
     db.commit()
 
+@app.route("/users")
 def add_user(name):
-    cursor.execute('''INSERT INTO users (name, points) VALUES
-                   ( ?, ?) ''', (name, 0))
-    db.commit()
-    print("Your ID is ", cursor.lastrowid)
+    try:
+        cursor.execute('''INSERT INTO users (name, points) VALUES
+                   (?, ?) ''', (name, 0))
+        db.commit()
+        print("Created the user!")
+    except:
+        print("This user already exists!")
 
-def increment_points(id):
-    cursor.execute('''UPDATE users SET points = points + 1 WHERE id=(?)''', (id,))
-    db.commit()
-    print("Points have been incrememented")
+def increment_points(name):
+    cursor.execute("SELECT COUNT(name) FROM users WHERE name=(?)", (name,))
+    count = cursor.fetchone()[0]
+    if count == 1:    
+        cursor.execute('''UPDATE users
+                   SET points = points + 1 
+                   WHERE name=(?)''', (name,))
+        db.commit()
+        print("Points have been incrememented")
+    else:
+        print("This name does not exist")
 
 def read_records():
     cursor.execute("SELECT * FROM users")
     rows = cursor.fetchall()
     for row in rows:
-        id = row[0]
-        name = row[1]
-        points = row[2]
-        print(f"{id} : {name} : {points}")
+        name = row[0]
+        points = row[1]
+        print(f"{name} : {points}")
+
+setup_database()
+add_user("Pranet")
+increment_points("Pranet")
+increment_points("null")
+
