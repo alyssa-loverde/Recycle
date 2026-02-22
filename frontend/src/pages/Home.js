04b1e7573b7b401ca2e0ccfd543d23a926715749
aasimon2 <aasimon2@illinois.edu>
2026-02-22 16:12:39 -0600

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,7 +3,7 @@ import '../ItemList.css';
 
 function Home(){
   return (
-    <div className="App">
+    <div className="ItemList">
         <h1>Learn How to Properly Recycle</h1>
         <p>Recycling does not have to be confusing. Our mission is to help individuals understand all about recycling and how easy it can be. Explore the codes, common items, and nearby drop-off locations to recycle responsibly.</p>
     </div>
