5a038c4cccafdeb68138293c2011ad6ac2fb2592
marc <msolc@illinois.edu>
2026-02-22 13:55:53 -0600

    buttons

--- recycle/src/App.js
+++ recycle/src/App.js
@@ -1,17 +1,28 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import './App.css';
 import Location from './pages/Location';
+import Item_list from './pages/Item_list';
+import Accounts from './pages/Accounts';
 
 function App() {
   return (
     <Router>
       <div>
         <Link to="/Location">
-          <button>Go to Location Page</button>
+          <button>Locations</button>
+        </Link>
+        <Link to="/Item_list">
+          <button>Item_list</button>
+        </Link>
+        <Link to="/Accounts">
+          <button>Accounts</button>
         </Link>
 
         <Routes>
+          <Route path="/" element={<Item_list />} />
           <Route path="/Location" element={<Location />} />
+          <Route path="/Item_list" element={<Item_list />} />
+          <Route path="/Accounts" element={<Accounts />} />
         </Routes>
       </div>
     </Router>
---
+++ recycle/src/pages/accounts.js
@@ -0,0 +1,14 @@
+import './Accounts.css';
+import React, { useState } from 'react';
+
+
+function Accounts() {
+  return (
+    <div className="Accounts">
+        <h1>Accounts</h1>
+
+    </div>
+  );
+}
+
+export default Accounts;
