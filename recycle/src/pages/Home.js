9496cee6d3b09ec0a067cf811f113884fad0adb0
aasimon2 <aasimon2@illinois.edu>
2026-02-22 15:12:46 -0600

    Item List and Home

---
+++ recycle/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../App.css';
+
+function Home(){
+  return (
+    <div className="App">
+        <h1 className="hero-title">Learn How to Properly Recycle</h1>
+        <p className="hero-subtitle code-info">Recycling does not have to be confusing. Our mission is to help individuals understand all about recycling and how easy it can be. Explore the codes, common items, and nearby drop-off locations to recycle responsibly.</p>
+    </div>
+  )
+}
+
+export default Home;
